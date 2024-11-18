6e2d015af0b89aa5d4b56e530109e5f524089230
alireza <alireza2000jadidi@gmail.com>
2024-11-18 14:40:49 -0500

    added yazi opener

--- scripts/work.sh
+++ scripts/work.sh
@@ -1,1 +1,1 @@
-hyprctl dispatch exec "[workspace 3; float; size 26% 23%; move 35% 10%;] kitty"
+hyprctl dispatch exec "[workspace $1; float; size 26% 23%; move 35% 10%;] kitty"
---
+++ scripts/yazi.sh
@@ -0,0 +1,1 @@
+hyprctl dispatch exec "[workspace $1; float; size 70% 70%;] kitty -e yazi"
