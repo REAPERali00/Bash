711ed487bb4b680795ced807fc99d4f37ec2ed74
Alireza <alireza2000jadidi@gmail.com>
2025-01-10 14:29:37 -0500

    fixed finding ics files in cal update

--- scripts/cal_updt.sh
+++ scripts/cal_updt.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+cd $HOME/Downloads/
 file="/home/alireza/Downloads/alireza18jadidi@gmail.com.ical.zip"
 
 unzip "$file"
 rm -rf "$file"
 
-for cal in /home/alireza/Downloads/*.ics; do
+for cal in $(find "$HOME/Downloads" -name "*.ics"); do
   khal import -a private --batch "$cal"
   if [ $? -eq 0 ]; then
     rm -rf "$cal"
