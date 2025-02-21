01d4c1188195d7b19166decc2efc565e3c682218
Alireza <alireza2000jadidi@gmail.com>
2025-02-21 14:44:56 -0500

    small update

--- scripts/load-state-1.sh
+++ scripts/load-state-1.sh
@@ -12,3 +12,5 @@ hyprctl dispatch exec "[workspace 1; float; size 48% 51%; move 25% 28%] kitty -e
 hyprctl dispatch exec "[workspace 1; float; size 87% 16%; move 7% 81%] kitty -e cava"
 hyprctl dispatch exec "[workspace 1; float; size 18.5% 15%; move 73.5% 63%;] kitty"
 sleep 4 # Give it time to launch
+
+clear
